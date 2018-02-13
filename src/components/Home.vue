a39821adb2a39d74fec2d2688b394cdf60a5dea3
Tuan-Anh BUI <tuananh.bui@euranova.eu>
2018-02-13 11:20:10 +0100

    Rename component HelloWorld -> Home

---
+++ src/components/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>My test navigation</h2>
+    <ul>
+      <li>
+        <router-link to='/test01'>Test01 raw</router-link> |
+        <router-link to='/test02'>Test02</router-link> |
+        <router-link to='/test02/userid/123'>Test02 with param</router-link> |
+        <router-link :to="{ name: 'Test01', params: { userid: 123 }}">Test01</router-link>
+      </li>
+    </ul>
+    <div>
+      <ul>
+        <li v-for="(user, uid) in users" v-bind:key="uid" >
+          {{user.firstname}} {{user.lastname}}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <input type="text" v-model="input_val">
+    </div>
+    This is the output: <span v-text="input_val"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to the awesome VueJS application',
+      users: [
+        {uid: 1, firstname: 'Sebastian', lastname: 'Eschweiler'},
+        {uid: 2, firstname: 'Bill', lastname: 'Smith'},
+        {uid: 3, firstname: 'John', lastname: 'Porter'}
+      ],
+      input_val: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: bold;
+  color: #0000FF;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
 import Test01 from '@/components/Test01'
 import Test02 from '@/components/Test02'
 
@@ -8,7 +8,8 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/', name: 'HelloWorld', component: HelloWorld },
+    { path: '/', name: 'home', component: Home },
+    { path: '/home', name: 'home', component: Home },
     { path: '/Test01', name: 'Test01', component: Test01 },
     { path: '/Test01/userid/:uid', name: 'Test01', component: Test01 },
     { path: '/test02', component: Test02 },
