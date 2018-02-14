a21f00cbcfaa87752fba71f68bc676fa4a065581
Tuan-Anh BUI <tuananh.bui@euranova.eu>
2018-02-14 22:57:17 +0100

    Add a globally registered component

---
+++ src/components/GlobalComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ header }}t</h1>
+    <div>
+        {{ body }}
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'globalcomponent',
+  data () {
+    return {
+      header: 'Global Component',
+      body: 'This component is registered globally'
+    }
+  }
+}
+</script>
