3b2fe28a1e91ce96a7537f0d41bbbedd0aec187f
Tuan-Anh BUI <tuananh.bui@euranova.eu>
2018-02-14 22:56:55 +0100

    Event handling

--- src/components/Test01.vue
+++ src/components/Test01.vue
@@ -1,10 +1,20 @@
 <template>
-    <div>
-        <h1>{{ header }}t</h1>
+  <div>
+    <h1>{{ header }}t</h1>
     <div>
         {{ body }}
     </div>
+    <div>
+      <button @click="doCount">Click</button>
+      Counting: {{ counter }}
     </div>
+    <div>
+      <button @click.stop="sayHello">Button 2</button>
+    </div>
+    <div>
+      Pops every five: {{ everyFivePop }}
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,7 +23,29 @@ export default {
   data () {
     return {
       header: 'This is the header of the Test01 component',
-      body: 'This is just some body text'
+      body: 'This is just some body text',
+      counter: 0,
+      name: 'whatever'
+    }
+  },
+  computed: {
+    everyFivePop: function () {
+      if (this.counter !== 0 && this.counter % 5 === 0) {
+        return 'pop'
+      } else {
+        return ''
+      }
+    }
+  },
+  methods: {
+    doCount: function () {
+      this.counter++
+    },
+    sayHello: function () {
+      alert('Hello world, ' + this.name)
+      if (event) {
+        alert(event.target.tagName)
+      }
     }
   }
 }
